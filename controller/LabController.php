f1ddf58833bd7208426b5a6dda74d6a81497c126
Diego Pozas <draulpozas@gmail.com>
2020-01-13 16:49:49 +0100

    nothing to do here atm

--- controller/LabController.php
+++ controller/LabController.php
@@ -3,7 +3,7 @@
 require_once __DIR__."/../config/autoload.php";
 
 class LabController{
-    
+    // nothing to do here atm
 }
 
  ?>
--- controller/UserController.php
+++ controller/UserController.php
@@ -3,7 +3,7 @@
 require_once __DIR__."/../config/autoload.php";
 
 class UserController{
-    
+    // nothing to do here atm
 }
 
  ?>
