f6908da6b68a4c5cdac88d9e8f3c79d398958333
Diego Pozas <draulpozas@gmail.com>
2020-01-14 14:11:10 +0100

    deleted unnecessary files

--- htdocs/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__."/../config/autoload.php";
-session_start();
-if (!isset($_SESSION['role']) || !isset($_SESSION['lab'])) {    
-    Login::main();
-}else{
-    View::init();
-}
-?>
--- htdocs/search.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once __DIR__."./../config/autoload.php";
-session_start();
-ReagentController::pageSearch();
